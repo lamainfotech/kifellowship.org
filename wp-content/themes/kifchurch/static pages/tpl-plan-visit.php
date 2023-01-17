53f0bd956244cf41bd9fff0aa736ae12c8a01364
Khagendra Lama <khagendralama2@gmail.com>
2023-01-18 05:35:47 +0545

    Static Two Pages Added

---
+++ wp-content/themes/kifchurch/static pages/tpl-plan-visit.php
@@ -0,0 +1,112 @@
+<?php
+/* Template Name: Static Plan A Visit */
+get_header();
+?>
+
+<section class="two-col text-left py-xxl-5 py-lg-4 py-sm-3 py-2">
+    <div class="container">
+        <div class="row justify-content-lg-between">
+            <div class="col-lg-6 col-xl-7 pr-lg-2 pr-xl-4 pr-xxl-5">
+                <div class="block-title">
+                    <h3>Welcome to KIF Service</h3>
+                    <p>We warmly welcome you to join us. We have our worship service in English at 10:00 AM every Saturday with tea/coffee and snacks. We are a group of people from different nations.</p>
+
+                    <p>We strive to create a vital community—with practical messages, heartfelt worship, prayer, justice, and mercy for those in need. It is our desire to cultivate a Christ-like lifestyle. Each of us is at various stages on our journey. Whether you’re a spiritual seeker who’s just starting to ask questions about God, or you already have a mature relationship with Christ, you will find a home here at Kifellowship.</p>
+
+                    <p>We observe a holy communion on the first Saturday of the month.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xl-4 pl-xxl-2">
+                <div class="block-title">
+                    <h3>KIF Kids</h3>
+                    <p>We believe that children are a vital part of our church body and if you are a family with kids- the KIF-KIDS join in singing & worship with the adults and are dismissed for their classes after the worship.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="info-block bg-primary py-1">
+    <div class="container">
+        <div class="info-block__desc">
+            <div class="icon-group mb-1 mb-lg-0">
+                <i class="icon-doc"></i>
+                <i class="icon-doc"></i>
+            </div>
+
+            <div class="info-block__content text-center">
+                <p><strong>Parking:</strong> available for bikes; cars can be parked at the pay and park area next to the building.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section bg-gray py-xl-4 py-lg-3 pt-sm-2 pb-sm-1 pt-2 pb-1">
+    <div class="container">
+        <div class="block-title text-center mb-1 mb-sm-2 mb-lg-3">
+            <h3>Nearby Attractions</h3>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-2">
+                <div class="card-overlay card-overlay--sm bg-cover dark-overlay">
+                    <img src="<?php echo site_url(); ?>/media/card-overlay-image-01.jpg" width="397" height="424" loading="lazzy" alt="Family">
+
+                    <div class="card-overlay__content text-center">
+                        <h3 class="h5">Garden</h3>
+                    </div>
+
+                    <div class="card-overlay__back text-center">
+                        <h3 class="h5">Garden</h3>
+                        <p>This is a beautiful garden in thamel, you can enjoy your day.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-2">
+                <div class="card-overlay card-overlay--sm bg-cover dark-overlay">
+                    <img src="<?php echo site_url(); ?>/media/card-overlay-image-01.jpg" width="397" height="424" loading="lazzy" alt="Family">
+
+                    <div class="card-overlay__content text-center">
+                        <h3 class="h5">Garden</h3>
+                    </div>
+
+                    <div class="card-overlay__back text-center">
+                        <h3 class="h5">Garden</h3>
+                        <p>This is a beautiful garden in thamel, you can enjoy your day.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-2">
+                <div class="card-overlay card-overlay--sm bg-cover dark-overlay">
+                    <img src="<?php echo site_url(); ?>/media/card-overlay-image-01.jpg" width="397" height="424" loading="lazzy" alt="Family">
+
+                    <div class="card-overlay__content text-center">
+                        <h3 class="h5">Garden</h3>
+                    </div>
+
+                    <div class="card-overlay__back text-center">
+                        <h3 class="h5">Garden</h3>
+                        <p>This is a beautiful garden in thamel, you can enjoy your day.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-2">
+                <div class="card-overlay card-overlay--sm bg-cover dark-overlay">
+                    <img src="<?php echo site_url(); ?>/media/card-overlay-image-01.jpg" width="397" height="424" loading="lazzy" alt="Family">
+
+                    <div class="card-overlay__content text-center">
+                        <h3 class="h5">Garden</h3>
+                    </div>
+
+                    <div class="card-overlay__back text-center">
+                        <h3 class="h5">Garden</h3>
+                        <p>This is a beautiful garden in thamel, you can enjoy your day.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
--- wp-content/themes/kifchurch/static pages/tpl-prayer-request.php
+++ wp-content/themes/kifchurch/static pages/tpl-prayer-request.php
@@ -3,6 +3,19 @@
 get_header();
 ?>
 
+<section class="section py-lg-4 py-sm-3 py-2">
+    <div class="container form-holder">
+        <header class="block-title block-width-85 mx-auto text-center mb-sm-2 mb-1">
+            <h2><span>Send Your</span>Prayer Request</h2>
+            <p>As a church, we are committed to seeing God work in our lives in very tangible ways. We know prayer is powerful.</p>
+        </header>
+        <div class="contact-form request-form">
+            <?php {
+                echo do_shortcode('[wpforms id="406' . $form_id . '" title="false"]');
+            } ?>
+        </div>
+    </div>
+</section>
 
 
 <?php
