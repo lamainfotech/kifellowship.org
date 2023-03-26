958f0591d6ce28363c44e134a9293ee69c3dbb99
Aashik81443 <56648807+Aashik81443@users.noreply.github.com>
2023-03-26 20:20:50 +0545

    beliefs page added

--- wp-content/themes/kifchurch/js/main.js
+++ wp-content/themes/kifchurch/js/main.js
@@ -164,6 +164,15 @@ var ss;
 		$('#' + "tab-" + dropdown).show();
 	});
 
+
+	$('.tab-menu li a').on('click', function () {
+		var target = $(this).attr('data-rel');
+		$('.tab-menu li a').removeClass('active');
+		$(this).addClass('active');
+		$("#" + target).fadeIn('slow').siblings(".tab-box").hide();
+		return false;
+	});
+
 })(jQuery);
 
 
